241f9165db20e535b3867af35f60daec043aeca6
ardydiaz <ardywerkz19@gmail.com>
2025-02-27 02:40:12 +0800

    exam

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\StudentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('student', function(){
-    return 'test';
-});
+Route::get('student', [StudentController::class, 'index']);
+Route::post('student/add', [StudentController::class, 'store']);
+Route::get('student/show/{id}', [StudentController::class, 'show']);
+Route::get('student/edit/{id}', [StudentController::class, 'edit']);
+Route::put('student/update/{id}', [StudentController::class, 'update']);
+Route::delete('student/delete/{id}', [StudentController::class, 'delete']);
